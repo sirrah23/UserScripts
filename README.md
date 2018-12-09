25c48b688909cdd7576d2b91d930c83c10d77719
sirrah23 <hseth93@gmail.com>
2018-12-09 02:49:14 -0500

    Enhance user script to open more manga categories and better README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Mangadex Popularity Seeker
+# User Scripts
 
-Creates buttons on the (Mangadex)[https://www.mangadex.org] website to quickly open links to popular manga.
+## Introduction
+
+A repository of potentially useful user scripts as used in [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://github.com/violentmonkey/violentmonkey).
+
+## How to use a user script
+
+1. Make sure you have either Tampermonkey or Violentmonkey installed
+2. Drag and drop the user script to your browser
+3. Navigate to the relevant URL
+
+## Scripts
+
+### manga.user.js
+
+Creates buttons on the [Mangadex](https://www.mangadex.org) website to quickly open popular manga.
--- manga.user.js
+++ manga.user.js
@@ -3,27 +3,51 @@
 // @namespace Violentmonkey Scripts
 // @match https://mangadex.org/
 // @grant none
+// @description Create buttons to open all popular manga from the following categories: past six hours, past day, and past week
 // ==/UserScript==
 
-$(document).ready(() => {
-  const hrefs = new Array();
-  const elements = $('#day ul li');
-  elements.each((i, elem) => {
-    hrefs.push(elem.firstElementChild.firstElementChild.href)
-  });
+class hrefContainer{
 
-  $('body').append('<input type="button" value="Open Links" id="OL">')
-  $("#OL").css("position", "fixed")
-          .css("top", 65)
+  constructor(xpath){
+    this.hrefs = new Array()
+    this.elements = $(xpath)
+    this.parseHREFs()
+  }
+
+  parseHREFs(){
+    for(let i = 0; i < this.elements.length; i++){
+      this.hrefs.push(this.elements[i].firstElementChild.firstElementChild.href)
+    }
+  }
+
+  getLinks(){
+    return this.hrefs
+  }
+}
+
+function createButton(container, text, id, idx){
+  $('body').append(`<input type="button" value="${text}" id="${id}">`)
+  $(`#${id}`).css("position", "fixed")
+          .css("top", 65+(idx*28))
           .css("left", 0)
           .css("background-color", "black")
           .css("color", "white")
           .css("border-radius", "12px")
-  $('#OL').click(function(){
-    $.each(hrefs, function(i, link) {
+  $(`#${id}`).click(function(){
+    $.each(container.getLinks(), function(i, link) {
       setTimeout(function(){
        window.open(link, '_blank');
       },1000);
     });
   });
+}
+
+
+$(document).ready(() => {
+  const sixContainer = new hrefContainer('#six_hours ul li')
+  const dayContainer = new hrefContainer('#day ul li')
+  const weekContainer = new hrefContainer('#week ul li')
+  createButton(sixContainer,  "six",  "SL", 0)
+  createButton(dayContainer,  "day",  "DL", 1)
+  createButton(weekContainer, "week", "WL", 2)
 });
